5b58e4e01a4d086ed8e79d0cf1cac5a131fed5b5
Haroon K M <154034759+haroontrailblazer@users.noreply.github.com>
2025-03-18 19:18:51 +0530

    cmt

--- Supervised Learning/Linear Regression/Linear regression2.py
+++ Supervised Learning/Linear Regression/Linear regression2.py
@@ -5,14 +5,14 @@ import numpy as np
 import matplotlib.pyplot as mp
 np.random.seed(0) 
 x = np.random.rand(100, 1) * 10 
-y = 2.5 * X.squeeze() + np.random.randn(100) * 2
+y = 2.5 * x.squeeze() + np.random.randn(100) * 2
 
 x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=42)
 
 x_train_sm=sm.add_constant(x_train)
 x_test_sm=sm.add_constant(x_test)
 
-model=sm.OLS(x_test_sm,y_train.fit())
+model=sm.OLS(y_train,x_train_sm).fit()
 y_pred=model.predict(x_test_sm)
 
 mse = mean_squared_error(y_test,y_pred)
