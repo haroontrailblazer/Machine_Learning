f9ebc37f452c8919e73c900b5166b7ac55dd5f29
Haroon K M <154034759+haroontrailblazer@users.noreply.github.com>
2025-06-01 17:33:19 +0530

    cmt

---
+++ Supervised Learning/Linear Regression/Math Model.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Create mple Dataset
+
+x = np.array([1500,750,1000,1250,1500,1750,2000,2250,2500,2750])
+y = np.array([100,150,200,230,270,310,340,38,420,450])
